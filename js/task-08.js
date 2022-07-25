b9f9c48e7c66760425545194921526fe92ed4add
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-07-25 23:46:57 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+  input: document.querySelectorAll("input"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value.length === 0 ||
+    event.currentTarget.elements.password.value.length === 0
+  ) {
+    return alert("Заполните все поля формы!");
+  }
+
+  let email = event.currentTarget.elements.email.value;
+  let password = event.currentTarget.elements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+
+  refs.input.textContent = "";
+}
