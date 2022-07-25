bb823c953d3b545388cf265a49427234ddccf32d
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-07-25 12:48:04 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,37 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// ************ forEach ************
+// const createItemsEl = (arr) => {
+//   const itemListEl = [];
+
+//   arr.forEach((el) => {
+//     const newItemIngredientsEl = document.createElement("li");
+//     newItemIngredientsEl.classList.add("item");
+//     newItemIngredientsEl.textContent = el;
+//     itemListEl.push(newItemIngredientsEl);
+//   });
+
+//   return itemListEl;
+// };
+
+// ************ map ************
+
+const createItemsEl = (arr) => {
+  return arr.map((x) => {
+    const newItemIngredientsEl = document.createElement("li");
+    newItemIngredientsEl.classList.add("item");
+    newItemIngredientsEl.textContent = x;
+    return newItemIngredientsEl;
+  });
+ 
+};
+
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.append(...createItemsEl(ingredients));
