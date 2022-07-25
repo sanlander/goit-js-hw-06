ebca1ff72e5d9022866b48c931b4cd2d129b3928
Alexandr Gorshkoder <S.gorshkoder@gmail.com>
2022-07-25 20:26:54 +0300

    +

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  buttonChangeColor: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  textColor: document.querySelector('.color'),
+};
+
+refs.buttonChangeColor.addEventListener('click', ()=> {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.textColor.innerHTML = getRandomHexColor();
+})
+
+
